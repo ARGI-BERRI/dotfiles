f8c4b61c577bf0fc8fca48dc97a1ac19c25e4289
ARGI-BERRI <me@endroit.net>
2024-07-28 21:57:12 +0900

    feat(alacritty): use byobu on startup, introducing nerd fonts
    Signed-off-by: ARGI-BERRI <me@endroit.net>

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ ARGIA's dotfiles
 ./install.sh
 ```
 
+You also need to install Monaspice from [Nerd Fonts](https://www.nerdfonts.com/font-downloads) to your system.
+
 ## License
 
 Apache-2.0
--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo apt update; sudo apt install -y \
     build-essential libssl-dev zlib1g-dev \
     libbz2-dev libreadline-dev libsqlite3-dev curl git \
     libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
-    jq bat duf fd-find fzf
+    jq bat duf fd-find fzf byobu
 
 # Install asdf
 export ASDF_DIR="$XDG_DATA_HOME"/asdf
