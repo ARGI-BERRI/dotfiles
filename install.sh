6b89d8d13c4d0403cbdf8be04a95147926f7855c
ARGI-BERRI <me@endroit.net>
2025-05-07 23:19:18 +0900

    feat: use mise

--- install.sh
+++ install.sh
@@ -5,56 +5,33 @@ export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_CACHE_HOME="$HOME/.cache"
-export CARGO_HOME="$XDG_DATA_HOME"/cargo
 
 PATH="$HOME/.local/bin:$PATH"
 
 if [ -e /etc/debian_version ]; then
-    sudo apt-get update
-    sudo apt-get install -y \
+    sudo add-apt-repository ppa:neovim-ppa/stable
+    sudo apt install -y \
         git curl wget zip unzip \
-        rustup gcc pipx \
-        neovim jq \
-        bat fd-find
-    rustup default stable
+        gcc pipx \
+        neovim
 
-    # Install cargo-binstall
-    curl -L --proto '=https' --tlsv1.2 -sSf \
-        https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh \
-        | bash > /dev/null 2>&1
+    # Install mise
+    curl -s https://mise.run | sh
+    ~/.local/bin/mise install aws bat chezmoi dust eza fzf jq starship zoxide
 
-    # Install utilities using cargo
-    cargo binstall -y du-dust zoxide eza
-
-    # Install chezmoi
-    sh -c "$(curl -fsLS get.chezmoi.io/lb)"
-
-    # Install starship
-    curl -sS https://starship.rs/install.sh | sh -s -- --yes
-
-    # Install uv
-    curl -LsSf https://astral.sh/uv/install.sh | sh
+    # Install uv and poetrymise ac
+    pipx install uv poetry mypy pytest ruff
     uv python install 3.12 3.13
-
-    # Install poetry
-    pipx install poetry
-
-    # Install fzf
-    if [ ! -d "$XDG_DATA_HOME"/fzf ]; then
-        git clone https://github.com/junegunn/fzf.git "$XDG_DATA_HOME"/fzf
-    fi
-    "$XDG_DATA_HOME"/fzf/install --bin --xdg
 fi
 
 if [ -e /etc/arch-release ]; then
     pacman -Syy
     pacman --noconfirm -Syu \
         git curl wget zip unzip \
-        rustup uv poetry gcc \
+        uv poetry gcc \
         neovim jq starship chezmoi \
-        bat fd dust zoxide eza fzf
+        bat dust zoxide eza fzf
 
-    rustup default stable
     uv python install 3.12 3.13
 fi
 
